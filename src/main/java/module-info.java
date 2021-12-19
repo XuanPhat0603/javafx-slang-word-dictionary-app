29e7b0a584fcbbc8de2822c7bbfbc12d33f62ea8
XuanPhat0603 <50132895+XuanPhat0603@users.noreply.github.com>
2021-12-19 23:33:30 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.dict_19127504 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.dict_19127504 to javafx.fxml;
+    exports com.example.dict_19127504;
+}
